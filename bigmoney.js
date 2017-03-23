8ea600c3bef84e220db292cd141ef0d3bc427c4c
Ryder Bergerud <ryder.bergerud@gmail.com>
2017-03-23 15:59:21 -0700

    Prettyprint query results, and added styling to results container.
    Rems are making text a littler smaller than they should be, as well as the legislature banner is a little too small (compared to the url https://dogwoodbc.ca/petitions/ban-big-money/).

--- bigmoney.js
+++ bigmoney.js
@@ -1,13 +1,14 @@
-stagingURL = "http://bigmoney-staging.bhga6ezash.us-west-2.elasticbeanstalk.com/";
-localURL = "http://localhost:8000/"
-endpoint = "api/donations/"
-queryData = {
-  min: '2500',
-}
+var stagingURL = "http://bigmoney-staging.bhga6ezash.us-west-2.elasticbeanstalk.com/";
+var localURL = "http://localhost:8000/";
+var endpoint = "api/donations/";
+var queryData = {
+                  min: '2500'
+                };
+
 $.getJSON(localURL+endpoint, queryData)
   .done((data) => {
-    $('#results').html(JSON.stringify(data));
+    document.getElementById('results').innerHTML = JSON.stringify(data, undefined, 2);
   })
   .fail((err) => {
     console.log(err);
-  })
+  });
