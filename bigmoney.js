c42db5bda211e333ec7a9251e378825835e8675b
Tana Jukes <tana.jukes@gmail.com>
2017-03-29 11:15:58 -0700

    Wire up contributor data display
    Pull contributor data from API and display it. No formatting, this is just to get the pieces connected.

--- bigmoney.js
+++ bigmoney.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 
 var localURL = "http://localhost:8000/";
 var donationsEndpoint = "api/donations/";
+var contributorsEndpoint = "api/contributors/";
 var donationsQuery = {
                   min: '2500'
+                };
+var contributorsQuery = {
+                  limit: '10'
                 };
 
 $.getJSON(localURL+donationsEndpoint, donationsQuery)
@@ -18,5 +22,15 @@ new Vue({
   el: '#test',
   data: {
     message: "This is a test"
+  }
+});
+
+
+var topTen = $.getJSON(localURL+contributorsEndpoint, contributorsQuery);
+
+new Vue({
+  el: '#topTen',
+  data: {
+    topTen: topTen
   }
 });
